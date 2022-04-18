c3f591a0dad2d4c41ed55a1a18e7d5ea3040e1b2
Alessandro Maciel <alessandromaciel237@gmail.com>
2022-04-18 11:36:55 -0300

    Fix controllers name in api routes

--- routes/api.php
+++ routes/api.php
@@ -8,11 +8,11 @@ Route::post('auth/login', 'API\AuthController@login');
 Route::group(['middleware' => ['apiJwt']], function () {
     Route::post('auth/logout', 'API\AuthController@logout');
 
-    Route::get('/tools', 'API\toolsController@index')->name('tools.index');
+    Route::get('/tools', 'API\ToolsController@index')->name('tools.index');
 
-    Route::get('/tools/{id}', 'API\toolsController@show')->name('tools.show');
+    Route::get('/tools/{id}', 'API\ToolsController@show')->name('tools.show');
 
-    Route::post('/tools', 'API\toolsController@store')->name('tools.store');
+    Route::post('/tools', 'API\ToolsController@store')->name('tools.store');
 
-    Route::delete('/tools/{id}', 'API\toolsController@destroy')->name('tools.delete');
+    Route::delete('/tools/{id}', 'API\ToolsController@destroy')->name('tools.delete');
 });
